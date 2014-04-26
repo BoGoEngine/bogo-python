e49113f77af0d906477db688fec8d00eba334f4a
Trung Ngo <ndtrung4419@gmail.com>
2014-04-26 13:04:53 +0700

    Introduce the keep_case decorator

--- bogo/test/test_utils.py
+++ bogo/test/test_utils.py
@@ -47,3 +47,62 @@ def test_separate():
     eq_(separate('xẻng'), ['x', 'ẻ', 'ng'])
     eq_(separate('xoáy'), ['x', 'oáy', ''])
     eq_(separate('quây'), ['qu', 'ây', ''])
+
+
+class TestKeepCase():
+    
+    def test_keep_lower(self):
+        
+        @keep_case
+        def function(string):
+            return string.upper()
+        
+        eq_(function("abc"), "abc")
+        
+    def test_keep_title(self):
+        
+        @keep_case
+        def function(string):
+            return string.upper()
+        
+        eq_(function("Abc"), "Abc")
+        
+    def test_keep_upper(self):
+        
+        @keep_case
+        def function(string):
+            return string.title()
+        
+        eq_(function("ABC"), "ABC")
+    
+    def test_multiple_arguments(self):
+        
+        @keep_case
+        def function(string, arg1, arg2, kwarg1=True):
+            return "{} {} {} {}".format(string, arg1, arg2, kwarg1)
+        
+        result = function("abc", 1, 2, 3)
+        expected = "abc 1 2 3"
+        
+        eq_(result, expected)
+        
+    def test_normalize_case(self):
+        """
+        Test that the string argument is always normalized to lower case.
+        """
+        inner = [0]
+        
+        @keep_case
+        def function(string):
+            inner[0] = string
+            return string
+        
+        function("ABC")
+        eq_(inner[0], "abc")
+        
+    def test_unrecognized_case(self):
+        @keep_case
+        def function(string):
+            return string
+        
+        eq_(function("aBcD"), "abcd")
